d081388697b7b33bac114129496db1d9bf97ac45
Alexandre Rivet <alex-rivet94@hotmail.fr>
2025-12-01 17:50:06 +0100

    feat: add negativeModulo utils

--- utils/index.ts
+++ utils/index.ts
@@ -4,3 +4,14 @@
  * @returns Array of file lines
  */
 export const inputToLines = (input: string) => input.trim().split('\n');
+
+/**
+ * Return positive a % b no matter what the sign of a
+ * @param {number} a number modulo
+ * @param {number} b number modulo from
+ * @returns {number} positive modulo
+ */
+export const negativeModulo = (a: number, b: number) => {
+    const mod = a % b;
+    return mod < 0 ? mod + b : mod;
+} 
