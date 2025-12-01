e0d8fb35e6f4d5a791a7cb8168cdc017fad54a47
Alexandre Rivet <alex-rivet94@hotmail.fr>
2025-12-01 23:28:27 +0100

    feat: solve day 1 part 2

--- days/day-01/part2/part.test.ts
+++ days/day-01/part2/part.test.ts
@@ -4,11 +4,11 @@ import * as fs from 'fs';
 describe('part 1', () => {
     it('example', async () => {
         const example = fs.readFileSync(__dirname + '/example.txt', 'utf-8');
-        expect(await part(example)).toBe(1);
+        expect(await part(example)).toBe(6);
     });
 
     it('input', async () => {
         const input = fs.readFileSync(__dirname + '/input.txt', 'utf-8');
-        expect(await part(input)).toBe(1);
+        expect(await part(input)).toBe(5887);
     });
 });
--- days/day-01/part2/part.ts
+++ days/day-01/part2/part.ts
@@ -1,9 +1,46 @@
-import { inputToLines } from '@utils';
+import { inputToLines, negativeModulo } from '@utils';
+import { getMoveOffset } from '../shared';
+
+const countZeroCrossings = (initialValue: number, offset: number, valuesCount) => {
+    // No movement
+    if (offset === 0) {
+        return 0;
+    }
+
+    // Positive movement
+    if (offset > 0) {
+        const stepsToDoAtLeastOneLoop = initialValue === 0 ? valuesCount : valuesCount - initialValue;
+        if (offset < stepsToDoAtLeastOneLoop) {
+            return 0;
+        }
+
+        // At least one loop + floored loops (retrieve first delta for first loop)
+        return 1 + Math.floor((offset - stepsToDoAtLeastOneLoop) / valuesCount);
+    }
+
+    // Negative movement
+    if (offset < 0) {
+        const stepsToDoAtLeastOneLoop = initialValue === 0 ? valuesCount : initialValue;
+        const absoluteOffset = Math.abs(offset);
+        if (absoluteOffset < stepsToDoAtLeastOneLoop) {
+            return 0;
+        }
+
+        // At least one loop + floored loops (retrieve first delta for first loop)
+        return 1 + Math.floor((absoluteOffset - stepsToDoAtLeastOneLoop) / valuesCount);
+    }
+
+}
 
 export const part = async (input: string): Promise<number> => {
     const lines = inputToLines(input);
+    const valuesCount = 100;
 
-    // @todo: to be implemented
-
-    return lines.length;
+    return lines.reduce((acc, v) => {
+        const offset = getMoveOffset(v);
+        return {
+            currentDial: negativeModulo(acc.currentDial + offset, valuesCount),
+            numOfZero: acc.numOfZero + countZeroCrossings(acc.currentDial, offset, valuesCount),
+        };
+    }, { currentDial: 50, numOfZero: 0 }).numOfZero;
 }
